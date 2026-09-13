c1275a8000570b2fb5e4f578822becf6bf9335cc
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-09-13 01:28:08 -0300

    add tandem queues example on main

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,11 @@ public class Main {
     public static void main(String[] args) {
         int count = 100000;
 
-        Interval arrival = new Interval(3, 5);
-        Interval departure = new Interval(4, 5);
-        Queue queue = new Queue(1, 5, arrival, departure);
-        Scheduler scheduler = new Scheduler();
+        Queue queue1 = new Queue(2, 3, new Interval(1, 5), new Interval(4, 5));
+        Queue queue2 = new Queue(1, 5, null, new Interval(1, 3));
 
-        Simulator simulator = new Simulator(List.of(queue), scheduler);
-        simulator.simulate(count, 3);
+        Scheduler scheduler = new Scheduler();
+        Simulator simulator = new Simulator(List.of(queue1, queue2), scheduler);
+        simulator.simulate(count, 2.5);
     }
 }
